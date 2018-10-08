7a86b1fa15be8034ec284b4a6e0618127699e796
Dzulmidnight <yasser.midnight@gmail.com>
2018-10-08 16:41:15 -0500

    modificación solicitudes

--- application/controllers/backend/administrador/solicitudes/SolicitudesPendientes.php
+++ application/controllers/backend/administrador/solicitudes/SolicitudesPendientes.php
@@ -2,10 +2,11 @@
 	class SolicitudesPendientes extends CI_Controller{
 		
 		function index(){
+			$this->load->view('backend/administrador/templates/head');
 			$this->load->view('backend/administrador/templates/header');
-			$this->load->view('backend/administrador/templates/topnavigation');
-			$this->load->view('backend/administrador/templates/navbar.php');
-			$this->load->view('backend/administrador/solicitudes/solicitudes_pendientes.php');
+			$this->load->view('backend/administrador/templates/sideNav');
+				$this->load->view('backend/administrador/solicitudes/solicitudes_pendientes');
+			$this->load->view('backend/administrador/templates/pre_footer');
 			$this->load->view('backend/administrador/templates/footer');
 		}
 	}
