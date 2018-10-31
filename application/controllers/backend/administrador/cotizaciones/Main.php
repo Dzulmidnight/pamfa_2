9759c0f2835d40b054193deb8372fb4e53b0a9b5
Dzulmidnight <yasser.midnight@gmail.com>
2018-10-30 21:07:15 -0600

    Proceso cotización

--- application/controllers/backend/administrador/cotizaciones/Main.php
+++ application/controllers/backend/administrador/cotizaciones/Main.php
@@ -11,7 +11,7 @@ class Main extends CI_Controller{
 		$this->load->view('backend/administrador/templates/head');
 		$this->load->view('backend/administrador/templates/header');
 		$this->load->view('backend/administrador/templates/sideNav');
-			$this->load->view('backend/administrador/cotizaciones/detalle_cotizacion');
+			$this->load->view('backend/administrador/cotizaciones/proceso_cotizacion');
 		$this->load->view('backend/administrador/templates/pre_footer');
 		$this->load->view('backend/administrador/templates/footer');
 	}
